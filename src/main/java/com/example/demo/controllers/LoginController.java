ba52b495a1c36b4f9e50b4276a13f4dd584df034
TienNH21 <tiennh21@fpt.edu.vn>
2024-01-22 13:53:45 +0700

    Tutor Buoi1: MVC, Get/Post-Mapping, RequestParam, Model

---
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.LoginRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    @GetMapping("login")
+    public String getLoginForm()
+    {
+        return "login";
+    }
+
+    @PostMapping("login")
+    public String login(LoginRequest req, Model model) {
+//        req.getParameter() -> String -> parseInt()
+        System.out.println("Đã vào hàm login()");
+        System.out.println(req.getUsername() + "-" + req.getPassword());
+        model.addAttribute("name", req.getUsername());
+        return "login";
+    }
+}
